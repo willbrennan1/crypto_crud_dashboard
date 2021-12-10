1732570cf0b04fc0745ddfa017d40d92e1f8d8ad
Will Brennan <willbrennan314@gmail.com>
2021-12-10 09:43:52 -0500

    Fetched API Data now showing in Table

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useMemo, useState, useEffect} from "react";
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import Home from './Home';
@@ -7,6 +8,54 @@ import Graphs from './Graphs';
 import NavBar from './NavBar';
 
 function App() {
+
+   //Setting state for API data
+   const [cells, setCells] = useState([])
+
+   var baseUrl = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+   var apiKey = "e1026657-301b-4947-9e98-e8982a94dbeb"
+
+   //Fetch request to retrieve API data
+   const getData = async () => {
+    const resp = await fetch(baseUrl, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CMC_PRO_API_KEY': apiKey,
+      }
+    })
+    .then(response => response.json())
+    .then(obj => {
+
+      let coinsData = obj.data;
+      setCells(coinsData)
+    })
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Rank",
+        accessor: "cmc_rank" // accessor is the "key" in the data
+      },
+      {
+        Header: "Name",
+        accessor: "name"
+      },
+      {
+        Header: "Symbol",
+        accessor: "symbol"
+      }
+    ],
+    []
+  );
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const data = React.useMemo(() => cells, []);
+
   return (
     <div>
       <NavBar />
@@ -21,7 +70,7 @@ function App() {
           <Graphs />
         </Route>
         <Route exact path ="/">
-          <Home />
+        <>{cells && <Home columns={columns} data={data} />}</>
         </Route>
       </Switch>
     </div>
